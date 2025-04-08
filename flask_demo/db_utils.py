aa3d6cf639f4871f3250fca4061aa8b1bbe07aee
Kristina <164719921+Kririyt@users.noreply.github.com>
2025-04-08 21:54:32 +0800

    Add files via upload

---
+++ flask_demo/db_utils.py
@@ -0,0 +1,23 @@
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    user='root',               # ← change to your username
+    password='abcd1234',        # ← 改成你的密码
+    database='optimal_samples',
+    charset='utf8mb4'
+)
+
+def save_to_db(identifier, groups):
+    try:
+        with conn.cursor() as cursor:
+            sql = "INSERT INTO results (identifier, sample_groups) VALUES (%s, %s)"
+            cursor.execute(sql, (identifier, str(groups)))
+        conn.commit()
+    except Exception as e:
+        print("Fail to write to Database：", e)
+
+def get_all_results():
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM results")
+        return cursor.fetchall()
